4ffbe64f368030507dec23d09a66b312378cc284
Nikk <phuc.nguyen@go1.com>
2018-12-25 15:02:21 +0700

    Support command only image

--- rootfs/start-react.sh
+++ rootfs/start-react.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-if [ ! -z "$REACT_WORKER_COUNT" ]; then
+if [ ! -z "$REACT_WORKER_COUNT" -a -z "$REACT_COMMAND_ONLY" ]; then
 	NEWLINE=$'\n'
 	react_sock="upstream reactor {${NEWLINE}"
 	for ((i=1; i<=$REACT_WORKER_COUNT; i++)); do
 	    react_sock+="    server unix:${REACT_WORKER_SOCK/\%s/${i}} fail_timeout=10;${NEWLINE}"
 	done
 	react_sock+="}"
-fi
 
-if [ ! -z "$react_sock" ]; then
 	echo "$react_sock"
-        echo "$react_sock" > /tmp/default.conf
-        cat /etc/nginx/sites-available/default.conf >> /tmp/default.conf
-        mv /tmp/default.conf /etc/nginx/sites-available/default.conf
+    echo "$react_sock" > /tmp/default.conf
+    cat /etc/nginx/sites-available/default.conf >> /tmp/default.conf
+    mv /tmp/default.conf /etc/nginx/sites-available/default.conf
 fi
 
 
@@ -23,10 +21,10 @@ if [ ! -z "$REACT_WORKER_COUNT" ]; then
 	done
 fi
 
-if [ ! -z "$REACT_WORKER_COUNT" ]; then
-        sleep ${REACT_WORKER_COUNT} 
+if [ ! -z "$REACT_WORKER_COUNT" -a -z "$REACT_COMMAND_ONLY" ]; then
+	sleep ${REACT_WORKER_COUNT} 
 	for ((i=1; i<=$REACT_WORKER_COUNT; i++)); do
-            chown www-data ${REACT_WORKER_SOCK/\%s/${i}}
+        chown www-data ${REACT_WORKER_SOCK/\%s/${i}}
 	done
 fi
 
